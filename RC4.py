7ecf83fa7e01c664f51c181a0ecc3503c7feb16e
kaltirim <kaltirim@lab.grid.auth.gr>
2015-03-24 15:37:13 +0200

    zip file exercise completed

--- RC4.py
+++ RC4.py
@@ -3,6 +3,6 @@
 import binascii
 
 message = "fasten your seat belt Dorothy, cause Kansas is going bye-bye"
-binary = bin(int(binascii.hexlify(message), 16))
+
 
 
--- ex7.py
+++ ex7.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 
-import zipfile
+import zipfile, os
 
 def extractFile(zFile, password):
 	try:
+		os.remove("test_zip")
+	except:
+		pass
+	try:
 		zFile.extractall(pwd=password)
-		print "Password is", password
-		return True
+		stats = os.stat('test_zip')
+		if (stats.st_size >= 59):
+			print "Password is", password
+			return True
+		else:
+			os.remove("test_zip")
 	except:
 		return False
 
 def main():
 	zFile = zipfile.ZipFile("test_zip.zip")
-	infile = open("english.txt", "r")
+	infile = open(u"english.txt", "r")
 	for password in infile:
-		password = password.strip('\n')
+		password = password.strip()
 		success = extractFile(zFile, password)
 		if success:
 			break
